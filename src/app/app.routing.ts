2a6cb4c485059f2ec446d0a3b025599716f781b9
tommysun10 <tommysun10@hotmail.com>
2018-08-17 18:38:39 -0400

    need to do api stuff later

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,7 +12,7 @@ const appRoutes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'admin', component: AdminComponent},
   { path: 'home', component: HomeComponent},
-  { path: 'recipe/create', component: RecipeMakerComponent},
+  { path: 'recipe-create', component: RecipeMakerComponent},
   { path: '**', component: HomeComponent}
 
 ];
--- src/app/recipe-maker/recipe-maker.component.ts
+++ src/app/recipe-maker/recipe-maker.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UserServiceClient} from '../services/user.service.client';
+import {RecipeServiceClient} from '../services/recipe.service.client';
+import { Router, ActivatedRoute } from "@angular/router";
+
 
 @Component({
   selector: 'app-recipe-maker',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-maker.component.css']
 })
 export class RecipeMakerComponent implements OnInit {
+	cuisineId = -1;
+	recipe = {
+		title: '',
+		description: '',
+		user: {},
+		ingredients: [],
+		imageURL: '',
+		youtubeVideo: '',
 
-  constructor() { }
+	};
+	user = {username:''};
+	ingredients = '';
+	youtube = {
+		imageURL: '',
+		video: '',
+	};
+
+  constructor(private userService: UserServiceClient,
+  	private recipeService: RecipeServiceClient,
+  	private router: Router,
+  	private aRoute: ActivatedRoute) { }
+
+  createRecipe() {
+  	this.recipe.ingredients = this.ingredients.split("\n");
+
+  	this.recipeService.createRecipe(this.cuisineId, this.recipe)
+  		.then(() => this.router.navigate(['home']));
+
+  }
 
   ngOnInit() {
+  	this.userService.currentUser()
+  		.then(user => this.user = user)
+  			.then(()=> {
+  				this.recipe.user = this.user.username;
+  			})
+  			.then(() => this.cuisineId = this.aRoute.snapshot.paramMap.get('cid')
+  				)
   }
 
 }
